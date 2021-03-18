5450e1e94372807aeb6d1c2e70d4184eca9e1696
SashankMeka1 <63683831+SashankMeka1@users.noreply.github.com>
2021-03-18 13:09:01 -0700

    Update room.h

--- room.h
+++ room.h
@@ -7,12 +7,12 @@ struct Item{
 	char name[10];
 	Item(const char * item_name){
 		strcpy(name, item_name);
-        }
+        }//item names
 };
 class Room{
 	public:
 		static char input[150];
-		map<char, Room*> exits;
+		map<char, Room*> exits;//inputs inventory room inventory and functions
 		vector<Item*> item_list;
 		static vector <Item*> inventory;
 		void printItem();
