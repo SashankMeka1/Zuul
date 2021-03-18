06c0f4d400e5802c9f61bb92849f1024199e01c8
SashankMeka1 <63683831+SashankMeka1@users.noreply.github.com>
2021-03-18 13:10:50 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 int main(){
 	Room* room;
-	Room room_matrix[4][4];
-	room_matrix[0][0].item_list.push_back(new Item("shovel"));
+	Room room_matrix[4][4];//4 by 4 matrix of rooms
+	room_matrix[0][0].item_list.push_back(new Item("shovel"));//add items to 00
 	room_matrix[0][0].item_list.push_back(new Item("bucket"));
 	room_matrix[0][0].item_list.push_back(new Item("sock"));
 	room_matrix[0][0].item_list.push_back(new Item("shoe"));	
 	room_matrix[0][0].item_list.push_back(new Item("book"));
 	for(int i = 0; i < 4; i++){
-		for(int j = 0; j <4; j++){
+		for(int j = 0; j <4; j++){//move adjacent rooms to map
 			if(i >0){
 				room_matrix[i][j].exits.insert(pair<char,Room*>('N',&room_matrix[i-1][j]));
 			}
@@ -28,7 +28,7 @@ int main(){
 		}
 	}
 	room = &room_matrix[0][0];
-	while(room){
+	while(room){//head to next room
 		room = room->nextRoom();
 	}
 	return 0;
